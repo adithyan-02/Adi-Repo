0d5586a1d06dd09a629f525dac80c1b53cea0894
mcsid5397 <64912029+mcsid5397@users.noreply.github.com>
2023-03-12 10:35:43 +0530

    Hello.java
    Understandable documentation on the source code

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
-public class Simple{
-public static void main(String args[]){
-System.out.println("hello javatpoint");
-}
+public class Simple
+{
+ public static void main(String args[])
+ {
+  System.out.println("hello javatpoint");
+ }
 }
